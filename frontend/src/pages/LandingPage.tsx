9698abcc237cb9670c614c8af372a1af6dd1176f
miljab <miloszjablonski@proton.me>
2025-10-14 16:36:37 +0200

    🐛 fix(oAuth): correct redirect URL after OAuth login
    - adjust redirect URLs in oAuthLogin controller to include a leading slash
    - add error handling to display toast message on landing page after failed authentication
    
    ✨ feat(frontend): add sonner for toast notifications
    
    - install sonner and next-themes for toast notifications and theme management
    - implement Toaster component with custom styling and icons
    - integrate Toaster in PublicLayout to display notifications

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -1,7 +1,20 @@
 import AuthOptions from "@/components/AuthOptions";
 import Hero from "@/components/Hero";
+import { useEffect } from "react";
+import { useSearchParams } from "react-router";
+import { toast } from "sonner";
 
 function LandingPage() {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  useEffect(() => {
+    if (searchParams.get("error") === "auth-error") {
+      toast.error("Error during authentication. Please try again.");
+      searchParams.delete("error");
+      setSearchParams(searchParams);
+    }
+  }, [searchParams, setSearchParams]);
+
   return (
     <div className="m-auto flex w-[93%] flex-col items-center justify-center gap-12 md:w-5/6 md:flex-row md:items-start md:gap-32 lg:gap-48">
       <Hero />
