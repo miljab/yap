847ea031955540fff698e1e4c77e46829e883bb2
miljab <miloszjablonski@proton.me>
2025-10-17 16:10:53 +0200

    ✨ feat(frontend): implement cancel onboarding dialog
    - add radix-ui dialog component for confirmation
    - create cancel onboarding dialog component with confirmation
    - replace cancel button with cancel onboarding dialog component

---
+++ frontend/src/components/CancelOnboardingDialog.tsx
@@ -0,0 +1,58 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+  DialogFooter,
+} from "./ui/dialog";
+import { Button } from "./ui/button";
+import { useNavigate } from "react-router";
+import axios from "@/api/axios";
+
+function CancelOnboardingDialog() {
+  const navigate = useNavigate();
+
+  const handleCancel = async () => {
+    try {
+      await axios.delete("/auth/onboarding/cancel", {
+        withCredentials: true,
+      });
+
+      navigate("/");
+    } catch (error) {
+      console.error(error);
+      // error page redirect
+    }
+  };
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="destructive" className="grow">
+          Cancel
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Do you want to cancel onboarding?</DialogTitle>
+          <DialogDescription>
+            This will reset your signing up process and you will have to start
+            over.
+          </DialogDescription>
+        </DialogHeader>
+        <DialogFooter>
+          <DialogTrigger asChild>
+            <Button variant="outline">Cancel</Button>
+          </DialogTrigger>
+          <DialogTrigger asChild>
+            <Button onClick={handleCancel}>Confirm</Button>
+          </DialogTrigger>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
+
+export default CancelOnboardingDialog;
