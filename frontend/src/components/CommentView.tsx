a5f52026dc494104dbd5ec855b7c0afc3695429f
miljab <miloszjablonski@proton.me>
2026-01-11 23:21:56 +0100

    feat(comment): add like/dislike functionality
    - implement like and dislike functionality for comments
    - manage like state and count locally for optimistic UI updates
    - integrate with comment like API endpoint
    - handle API errors by reverting state and displaying toast notifications
    - disable like button during API request to prevent multiple submissions

--- frontend/src/components/CommentView.tsx
+++ frontend/src/components/CommentView.tsx
@@ -4,12 +4,53 @@ import { AvatarFallback } from "@radix-ui/react-avatar";
 import defaultAvatar from "@/assets/default-avatar.png";
 import ImagePreview from "./ImagePreview";
 import { Heart, MessageCircleMore } from "lucide-react";
+import { useState } from "react";
+import useAxiosPrivate from "@/hooks/useAxiosPrivate";
+import { toast } from "sonner";
 
 type CommentViewProps = {
   comment: Comment;
 };
 
 function CommentView({ comment }: CommentViewProps) {
+  const [isLiking, setLiking] = useState(false);
+  const [isLiked, setLiked] = useState(comment.isLiked);
+  const [likeCount, setLikeCount] = useState(comment.likeCount);
+  const axiosPrivate = useAxiosPrivate();
+
+  async function handleLike() {
+    if (isLiking) return;
+
+    setLiking(true);
+    const prevLiked = isLiked;
+    const prevLikeCount = likeCount;
+
+    if (isLiked) {
+      setLiked(false);
+      setLikeCount((prev) => prev - 1);
+    } else {
+      setLiked(true);
+      setLikeCount((prev) => prev + 1);
+    }
+
+    try {
+      const { data } = await axiosPrivate.post(`/comment/${comment.id}/like`);
+      setLikeCount(data.likeCount);
+    } catch (error) {
+      console.error(error);
+
+      toast.error(
+        prevLiked
+          ? "Failed to dislike comment. Please try again."
+          : "Failed to like comment. Please try again",
+      );
+
+      setLiked(prevLiked);
+      setLikeCount(prevLikeCount);
+    } finally {
+      setLiking(false);
+    }
+  }
   return (
     <div className="flex flex-col gap-2 border p-2">
       <div className="flex items-center gap-1 text-sm">
@@ -34,11 +75,15 @@ function CommentView({ comment }: CommentViewProps) {
       </div>
 
       <div className="flex justify-start gap-4">
-        <button className="flex items-center gap-1">
+        <button
+          disabled={isLiking}
+          onClick={handleLike}
+          className="flex items-center gap-1"
+        >
           <Heart
-            className={`cursor-pointer transition-all duration-300 hover:text-red-500 ${comment.isLiked && "fill-red-500 text-red-500"}`}
+            className={`cursor-pointer transition-all duration-300 hover:text-red-500 ${isLiked && "fill-red-500 text-red-500"}`}
           />
-          {comment.likeCount > 0 && comment.likeCount}
+          {likeCount > 0 && likeCount}
         </button>
         <button className="flex items-center gap-1">
           <MessageCircleMore />
