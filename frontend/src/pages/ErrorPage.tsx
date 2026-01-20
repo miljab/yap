d5d4d491b9b9c26b287d3d39f67a9e0f5a86fdc5
miljab <miloszjablonski@proton.me>
2026-01-20 15:15:43 +0100

    feat(error-page): improve navigation options
    - add option to navigate back to the previous page
    - refactor navigation links to distinguish between previous and home pages

--- frontend/src/pages/ErrorPage.tsx
+++ frontend/src/pages/ErrorPage.tsx
@@ -1,10 +1,12 @@
-import { useLocation, Link } from "react-router";
+import { useLocation, Link, useNavigate } from "react-router";
 
 function ErrorPage() {
   const location = useLocation();
   const error =
     location.state?.error ||
     "Unknown error. Please try again, or contact support if the problem persists.";
+
+  const navigate = useNavigate();
 
   return (
     <div className="m-auto mt-12 flex w-9/12 flex-col gap-8">
@@ -17,9 +19,20 @@ function ErrorPage() {
         <p>{error}</p>
       </div>
 
-      <Link to="/" className="underline">
-        Go back to home page
-      </Link>
+      <div className="flex flex-col gap-4">
+        <span
+          className="cursor-pointer text-lg underline"
+          onClick={() => {
+            navigate(-1);
+          }}
+        >
+          Go back to the previous page
+        </span>
+
+        <Link to="/" className="underline">
+          Go to the home page
+        </Link>
+      </div>
     </div>
   );
 }
