fdea60c1a507a7dfe81db0789206d5f4218e44c6
miljab <miloszjablonski@proton.me>
2026-01-25 21:41:43 +0100

    feat(user): add default avatar on fetch
    - extend prisma client to compute default user avatar
    - if user avatar is null, use DEFAULT_AVATAR_URL from env

--- backend/prisma/prismaClient.ts
+++ backend/prisma/prismaClient.ts
@@ -1,9 +1,21 @@
+import "dotenv/config";
 import { PrismaClient } from "@prisma/client";
 
 export const prisma = new PrismaClient({
   omit: {
     user: {
       password: true,
+    },
+  },
+}).$extends({
+  result: {
+    user: {
+      avatar: {
+        needs: { avatar: true },
+        compute(user) {
+          return user.avatar ?? process.env.DEFAULT_AVATAR_URL!;
+        },
+      },
     },
   },
 });
