f75eafbce50b861f6dc8389aeaabb33453b900da
miljab <miloszjablonski@proton.me>
2026-02-15 19:43:04 +0100

    refactor(home-feed): split feed into tabbed components with all and following tabs

---
+++ frontend/src/components/post_components/HomeFeedFollowing.tsx
@@ -0,0 +1,9 @@
+function HomeFeedFollowing() {
+  return (
+    <div className="p-4 text-center text-neutral-500">
+      Following feed coming soon! Follow users to see their posts here.
+    </div>
+  );
+}
+
+export default HomeFeedFollowing;
