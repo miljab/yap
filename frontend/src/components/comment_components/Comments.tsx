556040a90609a9a7d2e0308d09fe08bdc2f0f114
miljab <miloszjablonski@proton.me>
2026-03-01 16:53:32 +0100

    refactor(post-view): optimize post and comment fetching - move comment fetching logic from Comments component to PostViewPage - fetch post and comments concurrently using Promise.all - simplify Comments component by making it purely presentational - introduce loading state for better user experience during data fetch

--- frontend/src/components/comment_components/Comments.tsx
+++ frontend/src/components/comment_components/Comments.tsx
@@ -1,39 +1,19 @@
 import CommentView from "./CommentView";
-import { useEffect, type SetStateAction } from "react";
-import useAxiosPrivate from "@/hooks/useAxiosPrivate";
 import type { Comment } from "@/types/post";
 
 type CommentsProps = {
-  postId: string;
   comments: Comment[];
-  setComments: React.Dispatch<SetStateAction<Comment[]>>;
+  setComments: React.Dispatch<React.SetStateAction<Comment[]>>;
   onCommentCreated: (newComment: Comment) => void;
   onCommentDeleted: () => void;
 };
 
 function Comments({
-  postId,
   comments,
   setComments,
   onCommentCreated,
   onCommentDeleted,
 }: CommentsProps) {
-  const axiosPrivate = useAxiosPrivate();
-
-  useEffect(() => {
-    const fetchComments = async () => {
-      try {
-        const response = await axiosPrivate.get(`/post/${postId}/comments`);
-
-        setComments(response.data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    fetchComments();
-  }, [postId, axiosPrivate, setComments]);
-
   if (comments.length === 0) return null;
 
   return (
