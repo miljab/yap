9a1a6d47f427c3c4141f757ef3fbad10715d507c
miljab <miloszjablonski@proton.me>
2026-03-05 15:17:56 +0100

    fix(profile): redirect on user not found
    - redirect to error page if user profile is not found (404)
    - provide clear feedback to user instead of displaying a blank page

--- frontend/src/pages/ProfilePage.tsx
+++ frontend/src/pages/ProfilePage.tsx
@@ -1,16 +1,18 @@
 import ProfileHeader from "@/components/user_components/ProfileHeader";
 import ProfileFeed from "@/components/user_components/ProfileFeed";
 import { useEffect, useState } from "react";
-import { useParams } from "react-router";
+import { useNavigate, useParams } from "react-router";
 import type { User } from "@/types/user";
 import useAxiosPrivate from "@/hooks/useAxiosPrivate";
 import useAuth from "@/hooks/useAuth";
+import type { AxiosError } from "axios";
 
 function ProfilePage() {
   const params = useParams();
   const [user, setUser] = useState<User | null>(null);
   const axiosPrivate = useAxiosPrivate();
   const { auth, setAuth } = useAuth();
+  const navigate = useNavigate();
 
   const onUserUpdate = (updatedUser: User) => {
     setUser(updatedUser);
@@ -30,11 +32,17 @@ function ProfilePage() {
         setUser(response.data);
       } catch (error) {
         console.error(error);
+        const axiosError = error as AxiosError;
+        if (axiosError.response?.status === 404) {
+          navigate("/error", {
+            state: { error: "User not found." },
+          });
+        }
       }
     };
 
     fetchData();
-  }, [axiosPrivate, params.username]);
+  }, [axiosPrivate, params.username, navigate]);
 
   if (!user) return null;
 
