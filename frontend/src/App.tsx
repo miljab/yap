118f7917fb0e3060898d241b3d07f332c8a11a77
miljab <miloszjablonski@proton.me>
2026-03-09 20:03:41 +0100

    feat(follow): add follow context provider
    - integrate FollowProvider to manage follow-related state
    - enable follow/unfollow functionality across the application

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,17 +3,20 @@ import { Routes, Route } from "react-router";
 import publicRoutes from "./routes/publicRoutes";
 import authRoutes from "./routes/authRoutes";
 import { AuthProvider } from "./context/AuthProvider";
+import { FollowProvider } from "./context/FollowProvider";
 import ErrorPage from "./pages/ErrorPage";
 
 function App() {
   return (
     <ThemeProvider defaultTheme="light" storageKey="uiTheme">
       <AuthProvider>
-        <Routes>{publicRoutes()}</Routes>
-        <Routes>{authRoutes()}</Routes>
-        <Routes>
-          <Route path="/error" element={<ErrorPage />} />
-        </Routes>
+        <FollowProvider>
+          <Routes>{publicRoutes()}</Routes>
+          <Routes>{authRoutes()}</Routes>
+          <Routes>
+            <Route path="/error" element={<ErrorPage />} />
+          </Routes>
+        </FollowProvider>
       </AuthProvider>
     </ThemeProvider>
   );
