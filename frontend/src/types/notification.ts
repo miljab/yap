87c89d9a923fe5e22dc0d094da3e9f02fcbfae6d
miljab <miloszjablonski@proton.me>
2026-04-16 22:25:54 +0200

    feat(notification): add notification type definition
    - create `Notification` type for frontend application
    - define structure for handling various notification events
    - include fields such as id, type, actor, read status, and related post or comment

---
+++ frontend/src/types/notification.ts
@@ -0,0 +1,15 @@
+export type Notification = {
+  id: string;
+  type:
+    | "LIKE_POST"
+    | "LIKE_COMMENT"
+    | "COMMENT_ON_POST"
+    | "REPLY_TO_COMMENT"
+    | "FOLLOW";
+  isRead: boolean;
+  actorCount: number;
+  actor: { id: string; username: string; avatarUrl: string } | null;
+  postId?: string;
+  commentId?: string;
+  createdAt: Date;
+};
