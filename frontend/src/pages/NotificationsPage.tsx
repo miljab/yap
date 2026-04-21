6c346c3ab68d626bc511cd3e59f7952dea5ffb3d
miljab <miloszjablonski@proton.me>
2026-04-21 21:24:26 +0200

    feat(notifications): add infinite scroll to notifications page
    - integrate `useInfiniteScroll` hook to handle fetching and displaying notifications
    - define `fetchNotifications` function to retrieve paginated notification data from the API
    - enable cursor-based pagination for efficient loading of notifications

--- frontend/src/pages/NotificationsPage.tsx
+++ frontend/src/pages/NotificationsPage.tsx
@@ -1,4 +1,30 @@
+import { axiosPrivate } from "@/api/axios";
+import { useInfiniteScroll } from "@/hooks/useInfiniteScroll";
+import type { Notification } from "@/types/notification";
+
 function NotificationsPage() {
+  const fetchNotifications = async (currentCursor?: string) => {
+    const url = currentCursor
+      ? `/notifications?cursor=${currentCursor}`
+      : "/notifications";
+
+    const response = await axiosPrivate.get(url);
+
+    return {
+      items: response.data.notifications,
+      nextCursor: response.data.nextCursor,
+    };
+  };
+
+  const {
+    items: notifications,
+    isLoading,
+    initialLoad,
+    loaderRef,
+    error,
+    retry,
+  } = useInfiniteScroll<Notification>(fetchNotifications);
+
   return <p>notifications</p>;
 }
 
